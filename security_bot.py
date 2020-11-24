9153ad2f66eae2bfb023979b41cf7b4341fdbaf8
juliushekkala <julius.hekkala@gmail.com>
2020-11-24 11:45:44 +0200

    Detect attachments

--- security_bot.py
+++ security_bot.py
@@ -15,6 +15,7 @@ async def on_ready():
     print(f'{client.user} has connected to Discord!')
     print(f'Guild is: {guild.name}, {guild.id}')
 
+''' On client message, the bot should check whether there are extensions and/or links '''
 @client.event 
 async def on_message(message):
     #ignore own messages
@@ -23,6 +24,16 @@ async def on_message(message):
     if message.content == "test":
         response = "Test succesful"
         await message.channel.send(response)
+    #for faster testing, exit when user types "bye"
+    if message.content == "bye":
+        exit(0)
+    #If there is an attachment
+    if message.attachments is not None:
+        for attachment in message.attachments:
+            print("\nAttachment detected\n")
+            print(attachment.filename)
+            #Handle attachments here
+
 
 
 client.run(TOKEN)
